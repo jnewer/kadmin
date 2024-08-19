ccd0d890902598ed0e1ca32e6c49248ac46e4ea3
jikang <jikang@jikangdeiMac.local>
2024-08-19 16:54:04 +0800

    update:#文件上传

--- app/controller/MediaController.php
+++ app/controller/MediaController.php
@@ -7,15 +7,26 @@ use support\Response;
 use app\actions\HasCrudActions;
 use app\service\MediaService;
 use app\controller\BaseController;
+use Tinywan\Jwt\JwtToken;
 
 class MediaController extends BaseController
 {
     use HasCrudActions;
-    
+
     protected MediaService $service;
 
     public function __construct(MediaService $service)
     {
         $this->service = $service;
+    }
+
+    public function create(Request $request): Response
+    {
+        $params = $request->all();
+        $params['file'] = current($request->file());
+        $params['admin_id'] = JwtToken::getCurrentId();
+        $this->service->create($params);
+
+        return success('操作成功');
     }
 }
